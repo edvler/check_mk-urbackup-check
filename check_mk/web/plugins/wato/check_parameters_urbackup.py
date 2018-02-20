3081084cab4c91afd56b42252dc66cd1088b8c36
Matthias Maderer <edvler@edvler-blog.de>
2018-02-20 18:46:30 +0100

    cleanup code

--- check_mk/web/plugins/wato/check_parameters_urbackup.py
+++ check_mk/web/plugins/wato/check_parameters_urbackup.py
@@ -1,3 +1,8 @@
+# Author: Matthias Maderer
+# E-Mail: edvler@edvler-blog.de
+# URL: https://github.com/edvler/check_mk_urbackup-check
+# License: GPLv2
+
 register_check_parameters(
     subgroup_os,
     "urbackup",
@@ -14,23 +19,23 @@ register_check_parameters(
                   ]
               )
 	    ),
-            ('backup_age',
-             Tuple(title = "Age of Backup before changing to warn or error",
-                   elements = [
-                       Age(title=_("Warning if backup is older than"), default_value = 93600, display = [ "minutes" ], help=_("If the backup is older as the specified time, the check changes to warning. (24h=1440m; 26h=1560m)")),
-                       Age(title=_("Critical if backup is older than"), default_value = 108000, display = [ "minutes" ], help=_("If the backup is older as the specified time, the check changes to warning. (24h=1440m; 26h=1560m)")),
-                   ]
-		  )
-            ),
             ( "modi",
               DropdownChoice(
-                  title = _("Use the status from UrBackup (see startpage) or use backup age for checks"),
+                  title = _("Use the status from UrBackup (see status Webpage in Urbackup), or use backup age for checks"),
                   help=_("Choose how the status of the backup should be determined"),
                   choices = [
-                     ( "use_backup_age",   _("Use the warning an critical limits given under backup_age") ),
-                     ( "use_urbackup_status",   _("Use the status of the UrBackup Status Page") ),
+                     ( "use_backup_age",   _("Backup age: Use the warning an critical limits given under backup_age") ),
+                     ( "use_urbackup_status",   _("UrBackup status: Use the status of the UrBackup Status Page") ),
                   ]
               )
+            ),
+            ('backup_age',
+             Tuple(title = "Age of Backup before changing to warn or error (this parameters are only used, if - Backup age - modi is choosen!)",
+                   elements = [
+                       Age(title=_("Warning at or above backupage"), default_value = 93600, display = [ "minutes" ], help=_("If the backup is older as the specified time, the check changes to warning. (24h=1440m; 26h=1560m)")),
+                       Age(title=_("Critical at or above backupage"), default_value = 108000, display = [ "minutes" ], help=_("If the backup is older as the specified time, the check changes to warning. (24h=1440m; 26h=1560m)")),
+                   ]
+		  )
             ),
 
         ]
