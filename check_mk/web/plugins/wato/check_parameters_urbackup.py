5f510fc2832c9e2dec343d75b49e5050dda028e7
Matthias Maderer <edvler@edvler-blog.de>
2018-02-22 19:02:21 +0100

    Version 1.0

--- check_mk/web/plugins/wato/check_parameters_urbackup.py
+++ check_mk/web/plugins/wato/check_parameters_urbackup.py
@@ -11,37 +11,37 @@ register_check_parameters(
         elements = [
             ("check_backup",
              DropdownChoice(
-                 title = _("Ignore or check the backup of this machine"),
-                 help=_("If this is set to ignore, the check will always be {ok}. To enable backup checks use the check option."),
+                 title = _("Enable (default) or disable check of backup"),
+                 help=_("If disabled is choosen, the check will always return OK. To enable checks of the backup, select enable. This is usefull if you have clients in UrBackup, for which no regular backups are done and you dont want them to be checked."),
                  choices = [
-                     ("ignore", _("ignore missing backups")),
-                     ("check", _("check for backups")),
+                     ("ignore", _("disable")),
+                     ("check", _("enable")),
                  ]
              )
             ),
             ("modi",
              DropdownChoice(
-                 title = _("Use the status from UrBackup (see status Webpage in Urbackup), or use backup age for checks"),
-                 help=_("Choose how the status of the backup should be determined"),
+                 title = _("Modi: use the status provided by UrBackup (see Status Webpage of Urbackup), or use backup age (default) as check criteria"),
+                 help=_("Choose how the status of the backup should be determined."),
                  choices = [
-                     ("use_backup_age", _("Backup age: Use the warning an critical limits given under backup_age")),
+                     ("use_backup_age", _("Backup age: Use the warning an critical limits given under Age of Backup")),
                      ("use_urbackup_status", _("UrBackup status: Use the status of the UrBackup Status Page")),
                  ]
              )
             ),
             ('backup_age',
              Tuple(
-                 title = "Age of Backup before changing to warn or error (this parameters are only used, if - Backup age - modi is choosen!)",
+                 title = "Age of Backup before changing to warn (default 26h) or error (default 30h). This parameters are only used, if modi Backup Age is choosen!",
                  elements = [
-                     Age(title=_("Warning at or above backupage"),
+                     Age(title=_("Warning at or above a backupage of"),
                          default_value = 93600,
                          display = [ "minutes" ],
-                         help=_("If the backup is older as the specified time, the check changes to warning. (24h=1440m; 26h=1560m)")
+                         help=_("If the backup is older than the specified time, the check changes to warning. (24h=1440m; 26h=1560m)")
                      ),
-                     Age(title=_("Critical at or above backupage"),
+                     Age(title=_("Critical at or above a backupage of"),
                          default_value = 108000,
                          display = [ "minutes" ],
-                         help=_("If the backup is older as the specified time, the check changes to warning. (24h=1440m; 26h=1560m)")
+                         help=_("If the backup is older than the specified time, the check changes to critical. (24h=1440m; 26h=1560m)")
                      ),
                  ]
              )
